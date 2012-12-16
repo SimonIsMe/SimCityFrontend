54d8c1bfb14a224e919e0080c90815c3ddb60700
Szymon Skrzyński <skrzynski.szymon@gmail.com>
2012-12-16 18:00:22 +0100

    api.js for heroku

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 Address = {
-//    apiAddress: 'http://localhost/simcity/api/',
-    api: 'http://localhost:3000/',
-//    apiAddress: 'http://radiant-cove-4147.herokuapp.com/',
-    img: 'http://localhost/simcity/img/',
+//    api: 'http://localhost:3000/',
+    api: 'http://radiant-cove-4147.herokuapp.com/',
+    img: 'http://lit-woodland-5595.herokuapp.com/img/'
+//    img: 'http://localhost/simcity/img/',
 }
